8dc47c8399cb3de6f89df95f79c9fbb7206d2fe2
Le Viet <leviet0115@gmail.com>
2021-02-16 12:17:14 +0200

    First commit

---
+++ Bookstore/src/main/java/server/Bookstore/domain/Book.java
@@ -0,0 +1,40 @@
+package server.Bookstore.domain;
+
+public class Book {
+	
+	protected String title;
+	protected String author;
+	protected String year;
+	protected String isbn;
+	protected double price;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+}
